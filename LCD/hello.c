a5215b6bac1967dd4f6b50b34d295547f873f067
turbocat2001 <maximlogaev2001ezro@gmail.com>
2020-10-29 21:46:44 +0300

    add LCD example

---
+++ LCD/hello.c
@@ -0,0 +1,20 @@
+﻿
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include "lcd/lcd-library.h"
+
+const char stringOne[] PROGMEM = "Hello world";
+const char stringTwo[] PROGMEM = "2 + 3 = 5";
+	
+int main(void)
+{
+	char i = 1;
+	char dir = LCD_SCROLL_LEFT;
+	
+	lcdInit();
+	lcdGotoXY(0,2);
+	lcdPutsf(stringOne);	
+	lcdGotoXY(1,3);	
+	lcdPutsf(stringTwo);
+
+}

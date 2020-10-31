5f43ecc44116b3831f5df871dc3583b0dce13cdf
turbocat2001 <maximlogaev2001ezro@gmail.com>
2020-10-31 22:53:18 +0300

    added custom symbols example

--- LCD/lcd_test.c
+++ LCD/lcd_test.c
@@ -3,18 +3,51 @@
 #include <avr/pgmspace.h>
 #include "lcd/lcd-library.h"
 
-const char stringOne[] PROGMEM = "Hello world";
-const char stringTwo[] PROGMEM = "2 + 3 = 5";
-	
+const char heart[] PROGMEM = { 
+	0b00000,
+	0b01010,
+	0b11111,
+  	0b11111,
+    0b11111,
+  	0b01110,
+  	0b00100,
+ 	0b00000
+};
+
+const char rus_p[] PROGMEM ={
+	0b11111,
+	0b10001,
+	0b10001,
+	0b10001,
+	0b10001,
+	0b10001,
+	0b10001,
+	0b00000,
+};
+
+const char rus_i[] PROGMEM = {
+	0b10001,
+	0b10001,
+	0b10011,
+	0b10101,
+	0b11001,
+	0b10001,
+	0b10001,
+	0b00000,
+};
+
+
+const char stringOne[] PROGMEM = "I \11 AVR";
+const char stringTwo[] PROGMEM = "\12P\3BET M\13P!";
+
 int main(void)
 {
-	char i = 1;
-	char dir = LCD_SCROLL_LEFT;
-	
 	lcdInit();
+	lcdLoadCharacterf(1, heart);
+	lcdLoadCharacterf(2, rus_p);
+	lcdLoadCharacterf(3, rus_i);
 	lcdGotoXY(0,2);
 	lcdPutsf(stringOne);	
-	lcdGotoXY(1,3);	
+	lcdGotoXY(1,2);	
 	lcdPutsf(stringTwo);
-
 }
